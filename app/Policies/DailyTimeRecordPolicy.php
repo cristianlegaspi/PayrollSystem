97f2c80d69fa1e062f4ff6aacd752c1f78521dbb
unknown <pjmesteves@tesda.gov.ph>
2026-03-17 16:22:05 +0800

    add policy

---
+++ app/Policies/DailyTimeRecordPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\DailyTimeRecord;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class DailyTimeRecordPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, DailyTimeRecord $dailyTimeRecord): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, DailyTimeRecord $dailyTimeRecord): bool
+    {
+         return in_array($user->role->role_name, ['Admin', 'Super Admin']);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, DailyTimeRecord $dailyTimeRecord): bool
+    {
+         return in_array($user->role->role_name, ['Admin', 'Super Admin']);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, DailyTimeRecord $dailyTimeRecord): bool
+    {
+         return in_array($user->role->role_name, ['Admin', 'Super Admin']);
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, DailyTimeRecord $dailyTimeRecord): bool
+    {
+        return in_array($user->role->role_name, ['Admin', 'Super Admin']);
+    }
+}
