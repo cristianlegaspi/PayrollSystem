1671fc5b632d9c0c15cd58baa691116b7805e1cb
unknown <pjmesteves@tesda.gov.ph>
2026-03-30 11:34:54 +0800

    adjust DTR

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->globalSearch(false)
             ->id('admin')
             ->path('admin')
             ->login()
